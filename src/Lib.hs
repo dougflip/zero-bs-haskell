71dade7595b88f44532ce660061114b2ebd9d4e0
Doug DiFilippo <ddifilippo@turnitin.com>
2019-06-23 21:52:04 -0400

    Exercise 02

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,11 @@ import qualified Zero.Server as Server
 helloHandler :: Server.Request -> Server.Response
 helloHandler _ = Server.stringResponse "hello"
 
+echoHandler :: Server.Request -> Server.Response
+echoHandler = Server.stringResponse . Server.requestBody
+
 run :: IO ()
 run = Server.startServer [
-    Server.simpleHandler Server.GET "/hello" helloHandler
+    Server.simpleHandler Server.GET "/hello" helloHandler,
+    Server.simpleHandler Server.POST "/echo" echoHandler
     ]
