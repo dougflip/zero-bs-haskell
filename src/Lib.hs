05137378198567711a4ecde290a713ecf9dc4547
Doug DiFilippo <ddifilippo@turnitin.com>
2019-06-23 22:00:54 -0400

    Exercise 03

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,8 +8,17 @@ helloHandler _ = Server.stringResponse "hello"
 echoHandler :: Server.Request -> Server.Response
 echoHandler = Server.stringResponse . Server.requestBody
 
+caseHandler :: Server.Request -> Server.Response
+caseHandler = Server.stringResponse . mapNumberToString . Server.requestBody
+
+mapNumberToString :: String -> String
+mapNumberToString "1" = "one"
+mapNumberToString "2" = "two"
+mapNumberToString "3" = "three"
+
 run :: IO ()
 run = Server.startServer [
     Server.simpleHandler Server.GET "/hello" helloHandler,
-    Server.simpleHandler Server.POST "/echo" echoHandler
+    Server.simpleHandler Server.POST "/echo" echoHandler,
+    Server.simpleHandler Server.POST "/case" caseHandler
     ]
